3ed7342ead2a957ae63e3558d77bc2f060671765
Gauri Mahangare <33997336+GauriMahangare@users.noreply.github.com>
2024-02-10 07:09:41 +0000

    Integration with rasa

---
+++ crs_web/static/js/constants.js
@@ -0,0 +1,4 @@
+const action_name = "action_hello_world";
+// Replace with Public IP address where Rasa server is hosted below
+const rasa_server_url = "http://localhost:5005/webhooks/rest/webhook";
+const sender_id = uuidv4();
