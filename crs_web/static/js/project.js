bd9f549d2e04ac909b015003d7261a7c2b0d9c57
Gauri Mahangare <33997336+GauriMahangare@users.noreply.github.com>
2024-02-10 02:01:33 +0000

    Changes for chat bot

--- crs_web/static/js/project.js
+++ crs_web/static/js/project.js
@@ -165,105 +165,105 @@ document.addEventListener("DOMContentLoaded", function () {
 Code for talk to me button
 */
 
-  document.querySelector("#open-chat-button").onclick = function (e) {
-    // add code for starting conversation
-    if (conversationId === "") {
-      payload = { status: "Active" };
+  // document.querySelector("#open-chat-button").onclick = function (e) {
+  //   // add code for starting conversation
+  //   if (conversationId === "") {
+  //     payload = { status: "Active" };
 
-      fetch(convURL, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          "X-CSRFTOKEN": getCookie("csrftoken"),
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({ agentId: 1 }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          conversationId = data.id;
-        });
-    }
-  };
+  //     fetch(convURL, {
+  //       method: "POST",
+  //       credentials: "same-origin",
+  //       headers: {
+  //         "X-Requested-With": "XMLHttpRequest",
+  //         "X-CSRFTOKEN": getCookie("csrftoken"),
+  //         "Content-type": "application/json",
+  //       },
+  //       body: JSON.stringify({ agentId: 1 }),
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         conversationId = data.id;
+  //       });
+  //   }
+  // };
 
-  sendButton.addEventListener("click", function () {
-    const messageText = messageInput.value.trim();
-    messageJSON = {
-      message: messageText,
-      conversationId: conversationId,
-    };
+  // sendButton.addEventListener("click", function () {
+  //   const messageText = messageInput.value.trim();
+  //   messageJSON = {
+  //     message: messageText,
+  //     conversationId: conversationId,
+  //   };
 
-    const chatSocket = new WebSocket(
-      "ws://" + window.location.host + "/ws/chat/" + roomName + "/"
-    );
+  //   const chatSocket = new WebSocket(
+  //     "ws://" + window.location.host + "/ws/chat/" + roomName + "/"
+  //   );
 
-    // Event handler for when the WebSocket connection is established
-    chatSocket.addEventListener("open", (event) => {
-      console.log("WebSocket connection opened:", event);
+  //   // Event handler for when the WebSocket connection is established
+  //   chatSocket.addEventListener("open", (event) => {
+  //     console.log("WebSocket connection opened:", event);
 
-      // Send a message to the WebSocket server
-      chatSocket.send(
-        JSON.stringify({
-          message: messageJSON,
-        })
-      );
-      if (messageText !== "") {
-        const messageDiv = document.createElement("div");
-        messageDiv.classList.add("message");
-        messageDiv.innerHTML = `
-                                <div class="d-flex flex-row justify-content-start mb-4">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="avatar 1" style="width: 45px; height: 100%;">
-                                <div class="p-3 ms-3" style="border-radius: 15px; background-color: rgba(57, 192, 237,.2);">
-                                    <p class="small mb-0">${messageText}</p>
-                                </div>
-                                </div>
-                            `;
-        chatMessages.appendChild(messageDiv);
-        messageInput.value = "";
-      }
-    });
-    // Event handler for when a message is received from the WebSocket server
-    chatSocket.addEventListener("message", (event) => {
-      console.log("Message from server:", event.data);
-      botMessage = JSON.parse(event.data);
-      // You can handle the received message here
-      if (event.data !== "") {
-        const messageDiv = document.createElement("div");
-        messageDiv.classList.add("message");
-        messageDiv.innerHTML = `
-            <div class="d-flex flex-row justify-content-end mb-4">
-            <div class="p-3 me-3 border" style="border-radius: 15px; background-color: #fbfbfb;">
-                <p class="small mb-0">${botMessage["message"]}</p>
-            </div>
-            <img src="/static/images/robot.png" alt="avatar 1" style="width: 45px; height: 100%;">
-            </div>
-                        `;
-        chatMessages.appendChild(messageDiv);
-        messageInput.value = "";
-      }
-    });
+  //     // Send a message to the WebSocket server
+  //     chatSocket.send(
+  //       JSON.stringify({
+  //         message: messageJSON,
+  //       })
+  //     );
+  //     if (messageText !== "") {
+  //       const messageDiv = document.createElement("div");
+  //       messageDiv.classList.add("message");
+  //       messageDiv.innerHTML = `
+  //                               <div class="d-flex flex-row justify-content-start mb-4">
+  //                               <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="avatar 1" style="width: 45px; height: 100%;">
+  //                               <div class="p-3 ms-3" style="border-radius: 15px; background-color: rgba(57, 192, 237,.2);">
+  //                                   <p class="small mb-0">${messageText}</p>
+  //                               </div>
+  //                               </div>
+  //                           `;
+  //       chatMessages.appendChild(messageDiv);
+  //       messageInput.value = "";
+  //     }
+  //   });
+  //   // Event handler for when a message is received from the WebSocket server
+  //   chatSocket.addEventListener("message", (event) => {
+  //     console.log("Message from server:", event.data);
+  //     botMessage = JSON.parse(event.data);
+  //     // You can handle the received message here
+  //     if (event.data !== "") {
+  //       const messageDiv = document.createElement("div");
+  //       messageDiv.classList.add("message");
+  //       messageDiv.innerHTML = `
+  //           <div class="d-flex flex-row justify-content-end mb-4">
+  //           <div class="p-3 me-3 border" style="border-radius: 15px; background-color: #fbfbfb;">
+  //               <p class="small mb-0">${botMessage["message"]}</p>
+  //           </div>
+  //           <img src="/static/images/robot.png" alt="avatar 1" style="width: 45px; height: 100%;">
+  //           </div>
+  //                       `;
+  //       chatMessages.appendChild(messageDiv);
+  //       messageInput.value = "";
+  //     }
+  //   });
 
-    // Event handler for handling WebSocket errors
-    chatSocket.addEventListener("error", (event) => {
-      console.error("WebSocket error:", event);
-    });
+  //   // Event handler for handling WebSocket errors
+  //   chatSocket.addEventListener("error", (event) => {
+  //     console.error("WebSocket error:", event);
+  //   });
 
-    // Event handler for when the WebSocket connection is closed
-    chatSocket.addEventListener("close", (event) => {
-      if (event.wasClean) {
-        console.log("WebSocket connection closed cleanly:", event);
-      } else {
-        console.error("WebSocket connection abruptly closed:", event);
-      }
-    });
-  });
+  //   // Event handler for when the WebSocket connection is closed
+  //   chatSocket.addEventListener("close", (event) => {
+  //     if (event.wasClean) {
+  //       console.log("WebSocket connection closed cleanly:", event);
+  //     } else {
+  //       console.error("WebSocket connection abruptly closed:", event);
+  //     }
+  //   });
+  // });
 
-  messageInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      sendButton.click();
-    }
-  });
+  // messageInput.addEventListener("keydown", function (event) {
+  //   if (event.key === "Enter") {
+  //     sendButton.click();
+  //   }
+  // });
   // movieInput.addEventListener("input", function () {
   //   var movieInputValue = movieInput.value;
   //   console.log(movieInputValue);
